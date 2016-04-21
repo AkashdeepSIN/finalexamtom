3b533d53cd46e05a1044ec1f2360c9e6b4dfc116
AkashdeepSIN <azzy.virk.13@gmail.com>
2016-04-21 10:52:10 -0400

    first commit

---
+++ Scripts/app.js
@@ -0,0 +1,22 @@
+function rollDice() {
+        var die1 = document.getElementById("die1");
+        var die2 = document.getElementById("die2");
+        var status = document.getElementById("status");
+        var d1 = Math.floor(Math.random() * 6) + 1;
+        var d2 = Math.floor(Math.random() * 6) + 1;
+        var diceTotal = d1 + d2;
+        die1.innerHTML = d1;
+        die2.innerHTML = d2;
+        status.innerHTML = "You rolled " + diceTotal+".";
+        if(d1 == d2) {
+            status.innerHTML += "Doubles! You get a free turn!!";
+        } else if (d1 === 3) {
+            status.innerHTML += "half life 3 confirmed";
+        } else if (d2 === 3)  {
+            status.innerHTML += "Half Life 3 confirmed";
+        } else if (d1 === 1) {
+
+        }
+console.log();
+}
+
