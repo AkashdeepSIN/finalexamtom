dbf2d8f91bb5f2b9cb881f7d5d7d4dd70cca4c34
AkashdeepSIN <azzy.virk.13@gmail.com>
2016-04-21 11:50:22 -0400

    updated

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,22 +1,33 @@
-// setup IIFE (Immediately Invoked Function Expression)
-function rollDice() {
-        var die1 = document.getElementById("die1");
-        var die2 = document.getElementById("die2");
-        var status = document.getElementById("status");
-        var d1 = Math.floor(Math.random() * 6) + 1;
-        var d2 = Math.floor(Math.random() * 6) + 1;
-        var diceTotal = d1 + d2;
-        die1.innerHTML = d1;
-        die2.innerHTML = d2;
-        status.innerHTML = "You rolled " + diceTotal+".";
-        if(d1 == d2) {
-            status.innerHTML += "Doubles! You get a free turn!!";
-        } else if (d1 === 3) {
-            status.innerHTML += "half life 3 confirmed";
-        } else if (d2 === 3)  {
-            status.innerHTML += "Half Life 3 confirmed";
-        } else if (d1 === 1) {
+/*app.js> Akashdeep Singh>Comp1073-Final Exam> 21/04/2016*/
+// Using images as var
+var face1=new Image()
+face1.src="../img/d1.gif"
+var face2=new Image()
+face2.src="../img/d2.gif"
+var face3=new Image()
+face3.src="../img/d3.gif"
+var face4=new Image()
+face4.src="../img/d4.gif"
+var face5=new Image()
+face5.src="../img/d5.gif"
+var face6=new Image()
+face6.src="../img/d6.gif"
 
-        }
-console.log();
+function throwdice(){
+	
+//create a random integer between 1 and 6
+	var randomdices=Math.round(Math.random()*6);
+
+// showing random images on click
+	document.images["mydice1"].src=eval("face"+randomdices+".src");
+
+//  var randomdices used in image and number result for same result
+	var die1 = document.getElementById("die1");
+	die1.innerHTML = randomdices;
+
+//create a random integer between 1 and 6
+	var randomdice=Math.round(Math.random()*6);
+	document.images["mydice2"].src=eval("face"+randomdice+".src");
+	var die2 = document.getElementById("die2");
+	die2.innerHTML = randomdice;
 }
